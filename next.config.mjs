25634d7fbade9bf05d75a275482a3f35ed7b2968
Ali Safarpour <safar2222@yahoo.com>
2024-08-10 20:19:58 +0330

    feat(project): Installed dependencies and init project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+const httpsHostnames = ['jsonplaceholder.typicode.com']
+const httpHostnames = ['api.localhost', '127.0.0.1',]
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            ...httpsHostnames.map(hostname => ({
+                protocol: 'https',
+                hostname
+            })),
+            ...httpHostnames.map(hostname => ({
+                protocol: 'http',
+                hostname
+            })),
+        ],
+    },
+};
 
 export default nextConfig;
